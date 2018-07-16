a486bc9227336aa6a1e5bc480709db0b6f790228
ROBIN ALEXANDER ROLFE <S14001664@mail.glyndwr.ac.uk>
2018-07-16 20:48:26 +0100

    Add project files.

---
+++ array_Values_Printed_In_Reverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_Values_Printed_In_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] valuesArray = { 2, 5, 7 };
+
+            Array.Reverse(valuesArray); // reverse the array
+
+            for (int i = 0; i < valuesArray.Length; i++)
+            {
+                Console.Write(valuesArray[i] + " ");
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
